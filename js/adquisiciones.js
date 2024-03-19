df8744b0ebb14de4b28e39ed38a279a97170131f
Brahyan Marquez <brayansma93@gmail.com>
2024-03-18 21:46:47 -0500

    Terminación de Crud de adquisicion

--- js/adquisiciones.js
+++ js/adquisiciones.js
@@ -1,21 +1,33 @@
 
 const api = "http://localhost/apiadres/public/"
-const ruta =  "adquisiciones"
-const host = api+ruta
+const ruta = "adquisiciones"
+const host = api + ruta
 
-function cargar(id, nombre,tipo) {
-    
+function cargar(id, presupuesto, fecha, valorUnitario, cantidad, valortotal, unidad, tipo, documentacion, proveedor) {
+
   $("#codigo").val(id);
-  $("#nombre").val(nombre);
+  $("#presupuesto").val(presupuesto);
+  $("#unidad").val(unidad);
   $("#tipo").val(tipo);
+  $("#cantidad").val(cantidad);
+  $("#valor_unitario").val(valorUnitario);
+  $("#valor_total").val(valortotal);
+  $("#fecha").val(fecha);
+  $("#proveedor").val(proveedor);
+  $("#documentacion").val(documentacion);
+
+
+
+
+
   $("#modificar").show();
   $("#guardar").hide();
 }
 
-function eliminar(id, nombre) {
+function eliminar(id, presupuesto, fecha, valorUnitario, cantidad, valortotal, unidad, tipo, documentacion, proveedor) {
   Swal.fire({
     title: "Ojo?",
-    html: `Esta seguro de Eliminar el Registro: <h3>${id} - ${nombre} !</h3>`,
+    html: `Esta seguro de Eliminar el Registro: <h3>${id} - ${fecha} - ${documentacion}!</h3>`,
     icon: "warning",
     showCancelButton: true,
     confirmButtonColor: "#3085d6",
@@ -24,7 +36,7 @@ function eliminar(id, nombre) {
   }).then((result) => {
     if (result.isConfirmed) {
       $.ajax({
-        url: host+"/" + id,
+        url: host + "/" + id,
         method: "DELETE",
         success: function (response) {
           cargarTabla();
@@ -44,7 +56,12 @@ function eliminar(id, nombre) {
 
 function Limpiar() {
   $("#codigo").val("");
-  $("#nombre").val("");
+  $("#presupuesto").val("");
+  $("#cantidad").val("");
+  $("#valor_unitario").val("");
+  $("#valor_total").val("");
+  $("#fecha").val("");
+  $("#documentacion").val("");
   $("#modificar").hide();
   $("#guardar").show();
 }
@@ -55,28 +72,84 @@ $("#limpiar").click(function () {
 
 $("#guardar").click(function () {
   let codigo = $("#codigo").val();
-  let nombre = $("#nombre").val();
+  let presupuesto = $("#presupuesto").val();
+  let unidad = $("#unidad").val();
   let tipo = $("#tipo").val();
+  let cantidad = $("#cantidad").val();
+  let valor_unitario = $("#valor_unitario").val();
+  let valor_total = $("#valor_total").val();
+  let fecha = $("#fecha").val();
+  let proveedor = $("#proveedor").val();
+  let documentacion = $("#documentacion").val();
 
-  if (nombre.trim() == "") {
+  if (presupuesto.trim() == "") {
     Swal.fire({
       title: "Ojo!",
-      html: "El campo <b>Nombre</b> esta vacio",
+      html: "El campo <b>Presupuesto</b> esta vacio",
       icon: "info",
     });
-  } else {
-    console.log("codigo: " + codigo + ", Nombre: " + nombre+ ", Tipo: " + tipo);
+  }
+  else if (cantidad.trim() == "") {
+    Swal.fire({
+      title: "Ojo!",
+      html: "El campo <b>Cantidad</b> esta vacio",
+      icon: "info",
+    });
+
+  }
+  else if (valor_unitario.trim() == "") {
+    Swal.fire({
+      title: "Ojo!",
+      html: "El campo <b>Valor Unitario</b> esta vacio",
+      icon: "info",
+    });
+
+  }
+  else if (valor_total.trim() == "") {
+    Swal.fire({
+      title: "Ojo!",
+      html: "El campo <b>Valor Total</b> esta vacio",
+      icon: "info",
+    });
+
+  }
+  else if (fecha.trim() == "") {
+    Swal.fire({
+      title: "Ojo!",
+      html: "El campo <b>Fecha Adquisición</b> esta vacio",
+      icon: "info",
+    });
+
+  }
+  else if (documentacion.trim() == "") {
+    Swal.fire({
+      title: "Ojo!",
+      html: "El campo <b>Documentación</b> esta vacio",
+      icon: "info",
+    });
+
+  }
+  else {
+
 
     let datos = {
-      codigo: codigo,
-      nombre: nombre,
-      categoria:tipo
+      "presupuesto": parseFloat(presupuesto),
+      "valor_unitario": parseFloat(valor_unitario),
+      "valor_total": parseFloat(valor_total),
+      "fecha_adquisicion": fecha,
+      "cantidad": parseInt(cantidad),
+      "unidad_id": parseInt(unidad),
+      "tipo_id": parseInt(tipo),
+      "documentacion": documentacion,
+      "proveedor_id": parseInt(proveedor)
     };
 
+    //console.log(datos);
     $.ajax({
-      url: host, 
+      url: host,
       method: "POST",
-      data: datos,
+      contentType: 'application/json',
+      data: JSON.stringify(datos),
       success: function (response) {
         Swal.fire({
           title: "Bien!",
@@ -102,7 +175,7 @@ $("#guardar").click(function () {
             console.error(xhr.responseJSON.error.nombre[0]);
             Swal.fire({
               title: "Mal!",
-              text: xhr.responseJSON.error.nombre[0],
+              text: xhr.responseJSON.error,
               icon: "error",
             });
           } else {
@@ -115,7 +188,7 @@ $("#guardar").click(function () {
             icon: "error",
           });
           console.error("Error al guardar el registro:", error);
-        } catch (error) {}
+        } catch (error) { }
       },
     });
   }
@@ -123,32 +196,82 @@ $("#guardar").click(function () {
 
 $("#modificar").click(function () {
   let codigo = $("#codigo").val();
-  let nombre = $("#nombre").val();
+  let presupuesto = $("#presupuesto").val();
+  let unidad = $("#unidad").val();
   let tipo = $("#tipo").val();
+  let cantidad = $("#cantidad").val();
+  let valor_unitario = $("#valor_unitario").val();
+  let valor_total = $("#valor_total").val();
+  let fecha = $("#fecha").val();
+  let proveedor = $("#proveedor").val();
+  let documentacion = $("#documentacion").val();
 
-  if (codigo.trim() == "") {
+  if (presupuesto.trim() == "") {
     Swal.fire({
       title: "Ojo!",
-      html: "El campo <b>Codigo</b> esta vacio",
+      html: "El campo <b>Presupuesto</b> esta vacio",
       icon: "info",
     });
-}else if (nombre.trim() == "") {
+  }
+  else if (cantidad.trim() == "") {
     Swal.fire({
       title: "Ojo!",
-      html: "El campo <b>Nombre</b> esta vacio",
+      html: "El campo <b>Cantidad</b> esta vacio",
       icon: "info",
     });
-  } else {
+
+  }
+  else if (valor_unitario.trim() == "") {
+    Swal.fire({
+      title: "Ojo!",
+      html: "El campo <b>Valor Unitario</b> esta vacio",
+      icon: "info",
+    });
+
+  }
+  else if (valor_total.trim() == "") {
+    Swal.fire({
+      title: "Ojo!",
+      html: "El campo <b>Valor Total</b> esta vacio",
+      icon: "info",
+    });
+
+  }
+  else if (fecha.trim() == "") {
+    Swal.fire({
+      title: "Ojo!",
+      html: "El campo <b>Fecha Adquisición</b> esta vacio",
+      icon: "info",
+    });
+
+  }
+  else if (documentacion.trim() == "") {
+    Swal.fire({
+      title: "Ojo!",
+      html: "El campo <b>Documentación</b> esta vacio",
+      icon: "info",
+    });
+
+  }
+  else {
+
+
     let datos = {
-      codigo: codigo,
-      nombre: nombre,
-      categoria: tipo,
+      "presupuesto": parseFloat(presupuesto),
+      "valor_unitario": parseFloat(valor_unitario),
+      "valor_total": parseFloat(valor_total),
+      "fecha_adquisicion": fecha,
+      "cantidad": parseInt(cantidad),
+      "unidad_id": parseInt(unidad),
+      "tipo_id": parseInt(tipo),
+      "documentacion": documentacion,
+      "proveedor_id": parseInt(proveedor)
     };
-
     $.ajax({
-      url: host+"/" + codigo, 
+      url: host + "/" + codigo,
       method: "PUT",
-      data: datos,
+      contentType: 'application/json',
+      data: JSON.stringify(datos),
       success: function (response) {
         Swal.fire({
           title: "Bien!",
@@ -160,12 +283,12 @@ $("#modificar").click(function () {
         Limpiar();
       },
       error: function (xhr, status, error) {
+        console.error("Error al Actualizar el registro:", error);
         Swal.fire({
           title: "Mal!",
           text: response.message,
           icon: "error",
         });
-        console.error("Error al Actualizar el registro:", error);
       },
     });
   }
@@ -181,9 +304,9 @@ $(document).ready(function () {
   $.ajax({
     url: unidad,
     method: "GET",
-    success: function(resultado) {
+    success: function (resultado) {
       // console.log(resultado['data']);
-      $.each(resultado['data'], function(index, unidad) {
+      $.each(resultado['data'], function (index, unidad) {
         $("#unidad").append(
           `<option value="${unidad.id}">${unidad.nombre}</option>`
         );
@@ -195,9 +318,9 @@ $(document).ready(function () {
   $.ajax({
     url: tipos,
     method: "GET",
-    success: function(resultado) {
+    success: function (resultado) {
       // console.log(resultado['data']);
-      $.each(resultado['data'], function(index, tipo) {
+      $.each(resultado['data'], function (index, tipo) {
         $("#tipo").append(
           `<option value="${tipo.id}">${tipo.nombre} - ${tipo.categoria}</option>`
         );
@@ -209,9 +332,9 @@ $(document).ready(function () {
   $.ajax({
     url: proveedores,
     method: "GET",
-    success: function(resultado) {
+    success: function (resultado) {
       // console.log(resultado['data']);
-      $.each(resultado['data'], function(index, proveedor) {
+      $.each(resultado['data'], function (index, proveedor) {
         $("#proveedor").append(
           `<option value="${proveedor.id}">${proveedor.nombre}</option>`
         );
@@ -219,7 +342,16 @@ $(document).ready(function () {
     },
   });
 
-
+  $('#cantidad, #valor_unitario').on('input', function () {
+    let cantidad = parseFloat($('#cantidad').val());
+    let valorUnitario = parseFloat($('#valor_unitario').val());
+    if (!isNaN(cantidad) && !isNaN(valorUnitario)) {
+      let valorTotal = cantidad * valorUnitario;
+      $('#valor_total').val(valorTotal);
+    } else {
+      $('#valor_total').val('');
+    }
+  });
 
   $("#modificar").hide();
   $("#tablaAdquisiciones").DataTable({
@@ -233,13 +365,12 @@ $(document).ready(function () {
     pageLength: 5,
     "columnDefs": [
       {
-          "targets": [1,3,5], // Índice de la tercera columna (0-indexed)
-          "render": function(data, type, row) {
-              // Convertir el número a un número entero y luego formatearlo como cadena
-              return parseInt(data).toString();
-          }
+        "targets": [1, 3, 5],
+        "render": function (data, type, row) {
+          return parseInt(data).toString();
+        }
       }
-  ],
+    ],
     columns: [
       { data: "id" },
       { data: "presupuesto" },
@@ -249,6 +380,7 @@ $(document).ready(function () {
       { data: "valor_total" },
       { data: "unidad_id" },
       { data: "tipo_id" },
+      { data: "documentacion" },
       { data: "proveedor_id" },
       {
         data: null,
@@ -271,9 +403,12 @@ $(document).ready(function () {
             "', '" +
             data.tipo_id +
             "', '" +
-            data.proveedor_id +
+            data.documentacion +
+            "', '" +
+            data.proveedor_id
+            +
             '\')"><i class="mdi mdi-magnify mdi-size-lg" style="font-size: 20px;"></i></button>' +
-            '<button class ="btn btn-danger" onclick="eliminar((\'' +
+            '<button class ="btn btn-danger" onclick="eliminar(\'' +
             data.id +
             "', '" +
             parseFloat(data.presupuesto).toFixed(0) +
@@ -290,7 +425,10 @@ $(document).ready(function () {
             "', '" +
             data.tipo_id +
             "', '" +
-            data.proveedor_id +
+            data.documentacion +
+            "', '" +
+            data.proveedor_id
+            +
             '\')"><i class="mdi mdi-delete mdi-size-lg" style="font-size: 20px;"></i></button>'
           );
         },
